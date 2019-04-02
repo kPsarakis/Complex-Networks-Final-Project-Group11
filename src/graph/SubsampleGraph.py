53d28bd775c81827fd2ed9b4e44b52ea49dd6aa0
Berend Baas <berendbaas@gmail.com>
2019-04-02 16:57:28 +0200

    Adding random subsampling for graphs

--- src/graph/SubsampleGraph.py
+++ src/graph/SubsampleGraph.py
@@ -1,42 +1,35 @@
 import networkx as nx
-
-def random_connected_node_selection(graph, N):
-    # Select a random node from the nodelist and add it to the set
-    pass
+import Graph
 
 # @TODO: Change into random node set returned (and make a seperate method for constructing the graph)
-def random_subample(graph, N):
+def random_connected_subgraph(graph, N):
     # Select a random node from the nodelist and add it to the set
-    current_nodes = set()
-    outermost_nbs = set()
+    starting_node = Graph.random_node_id(list(graph.nodes))
+    print(starting_node)
+
+    current_nodes = set([starting_node])
+    outermost_nbs = set([starting_node])
     new_outermost_nbs = set()
 
-    while True:
+    need_more_nodes = True
+    while need_more_nodes:
         for n in outermost_nbs:
             # Fetch outgoing edges from this node
-            nbs = G.adj[n]
+            nbs = graph.adj[n]
 
             # Add all to new neighbours.
-            new_outermost_nbs.union(nbs)
-
+            new_outermost_nbs.update(list(nbs))
             # Extend sets
-            # current_nodes.
+            current_nodes.update(new_outermost_nbs)
 
             if len(current_nodes) > N:
-                break;
+                need_more_nodes = False
+                break
 
-        outermost_nbs = new_outermost_nbs
-
+        outermost_nbs = set(new_outermost_nbs)
         if len(outermost_nbs) == 0:
             print("Fully explored connected component")
-            break;
+            break
 
     # Conststruct subgraph from current_nodes
-
-    pass
-
-
-# @TODO: Rename
-def random_subsample_2():
-    # Perform random_subsample(), if it doesn't match, then remove all these nodes from the graph
-    pass
+    return graph.subgraph(list(current_nodes))
