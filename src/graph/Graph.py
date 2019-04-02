d6ba290458769e47d482b5380a8d2996bc552e63
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-04-02 14:47:11 +0200

    Pickle addition to the graph generation

--- src/graph/Graph.py
+++ src/graph/Graph.py
@@ -1,29 +1,50 @@
 import pandas as pd
 import networkx as nx
 import random
+from pathlib import Path
 import src.graph.Metrics as Met
 
-df = pd.read_csv('../../data/processed/products_nodes_links.csv', sep=';')
-df.columns = ['node1', 'node2']
 
-G = nx.Graph()
-
-for i in range(0, len(df.index)):
-    G.add_edge(str(df.node1[i]), str(df.node2[i]))
-
-nodes = G.nodes
-
-Met.print_metrics(G)
+def random_walk(graph):
+    # node_couts = {}
+    return graph
 
 
-def random_walk(g):
-    node_couts = {}
-    return g
+def random_node_id(nodes):
+    return random.choice(nodes)
 
 
-def random_node_id(n):
-    return random.choice(n)
+def next_step(node, graph):
+    print(graph.edges(node))
+    print(len(graph.edges(node)))
+    print(1/len(graph.edges(node)))
+    return graph.edges(node)
 
 
-def adjnodes():
-    return 0
+def initialize_graph():
+    my_file = Path("../../data/graph.p")
+
+    if my_file.is_file():
+        graph = nx.read_gpickle(my_file)
+    else:
+        df = pd.read_csv('../../data/processed/products_nodes_links.csv', sep=';')
+        df.columns = ['node1', 'node2']
+
+        graph = nx.Graph()
+        for i in range(0, len(df.index)):
+            graph.add_edge(str(df.node1[i]), str(df.node2[i]))
+
+        nx.write_gpickle(graph, my_file)
+
+    return graph
+
+
+if __name__ == '__main__':
+
+    g = initialize_graph()
+
+    n = list(g.nodes)
+
+    print(random_node_id(n))
+
+    Met.print_metrics(g)
